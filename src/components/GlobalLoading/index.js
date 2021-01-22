e2af124934648cf80fd174981b323aea1d3676c5
trieuquang <trieuquang071997@gmail.com>
2021-01-22 15:33:52 +0700

    134 hien thi loading

--- src/components/GlobalLoading/index.js
+++ src/components/GlobalLoading/index.js
@@ -3,20 +3,42 @@ import { withStyles } from "@material-ui/styles";
 import styles from "./styles";
 import LoadingIcon from "../../assets/images/loading.gif";
 import PropTypes from "prop-types";
+import { bindActionCreators, compose } from "redux";
+import { connect } from "react-redux";
+import * as uiActions from "../../actions/ui";
 
 class GlobalLoading extends Component {
   render() {
-    const { classes } = this.props;
-    return (
-      <div className={classes.globalLoading}>
-        <img src={LoadingIcon} alt="logo" className={classes.icon} />
-      </div>
-    );
+    const { classes, showLoading } = this.props;
+    let xhtml = null;
+    if (showLoading) {
+      xhtml = (
+        <div className={classes.globalLoading}>
+          <img src={LoadingIcon} alt="logo" className={classes.icon} />
+        </div>
+      );
+    }
+    return xhtml;
   }
 }
 
 GlobalLoading.propTypes = {
   classes: PropTypes.object,
+  showLoading: PropTypes.bool,
 };
 
-export default withStyles(styles)(GlobalLoading);
+const mapStateToProps = (state) => {
+  return {
+    showLoading: state.ui.showLoading,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    uiActions: bindActionCreators(uiActions, dispatch),
+  };
+};
+
+const withConnect = connect(mapStateToProps, mapDispatchToProps);
+
+export default compose(withStyles(styles), withConnect)(GlobalLoading);
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,12 +1,14 @@
-import { call, fork, take, put } from "redux-saga/effects";
+import { call, fork, take, put, delay } from "redux-saga/effects";
 import { fetchListTaskFailed, fetchListTaskSuccess } from "../actions/task";
 import { getList } from "../apis/task";
 import { STATUS_CODE } from "../constants";
 import * as taskTypes from "../constants/task";
+import { showLoading, hideLoading } from "../actions/ui";
 
 function* watchFetchListTaskAction() {
   while (true) {
     yield take(taskTypes.FETCH_TASK);
+    yield put(showLoading());
     const resp = yield call(getList);
     const { status, data } = resp;
     if (status === STATUS_CODE.SUCCESS) {
@@ -16,6 +18,8 @@ function* watchFetchListTaskAction() {
       // dispatch action fietlistTaskFailed
       yield put(fetchListTaskFailed(error));
     }
+    yield delay(1000);
+    yield put(hideLoading());
   }
 }
 
